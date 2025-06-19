7821bf8ac4a1f909988238a699b795a8746c2362
alexchebotarsky <alexchebotarsky@gmail.com>
2025-06-19 14:11:05 +0200

    Remove account field from post type

--- mastodon-aggregator/model/post/post.go
+++ mastodon-aggregator/model/post/post.go
@@ -1,18 +1,9 @@
 package post
 
 type Post struct {
-	ID        string   `json:"id"`
-	CreatedAt string   `json:"created_at"`
-	Language  string   `json:"language"`
-	URL       string   `json:"url"`
-	Content   string   `json:"content"`
-	Account   *Account `json:"account"`
-}
-
-type Account struct {
-	ID          string `json:"id"`
-	Username    string `json:"username"`
-	DisplayName string `json:"display_name"`
-	Acct        string `json:"acct"`
-	URL         string `json:"url"`
+	ID        string `json:"id"`
+	CreatedAt string `json:"created_at"`
+	URL       string `json:"url"`
+	Language  string `json:"language"`
+	Content   string `json:"content"`
 }
