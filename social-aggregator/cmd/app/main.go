00972abde0212e54f94fcf0830fae62457eeb9a1
alexchebotarsky <alexchebotarsky@gmail.com>
2025-06-19 09:38:05 +0200

    Setup simple server for social-aggregator

---
+++ social-aggregator/cmd/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/alexchebotarsky/social/social-aggregator/app"
+	"github.com/alexchebotarsky/social/social-aggregator/env"
+)
+
+func main() {
+	ctx := context.Background()
+
+	env, err := env.LoadConfig(ctx)
+	if err != nil {
+		log.Printf("Error loading env config: %v", err)
+		os.Exit(1)
+	}
+
+	app, err := app.New(ctx, env)
+	if err != nil {
+		log.Printf("Error creating app: %v", err)
+		os.Exit(1)
+	}
+
+	app.Launch(ctx)
+}
---
+++ social-media-aggregator/service/server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexchebotarsky/social/social-media-aggregator/service/server/handler"
+)
+
+type Server struct {
+	Host    string
+	Port    uint16
+	Router  *http.ServeMux
+	HTTP    *http.Server
+	Clients Clients
+}
+
+type Clients struct {
+	Database DatabaseClient
+}
+
+type DatabaseClient interface {
+	handler.PostsSelector
+}
+
+func New(host string, port uint16, clients Clients) *Server {
+	var s Server
+
+	s.Host = host
+	s.Port = port
+	s.Router = http.NewServeMux()
+	s.HTTP = &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", s.Host, s.Port),
+		Handler:      s.Router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	s.Clients = clients
+
+	s.setupRoutes()
+
+	return &s
+}
+
+func (s *Server) Start(ctx context.Context, errc chan<- error) {
+	log.Printf("Server is listening at %s:%d", s.Host, s.Port)
+	err := s.HTTP.ListenAndServe()
+	if err != http.ErrServerClosed {
+		errc <- fmt.Errorf("error listening and serving: %v", err)
+	}
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	err := s.HTTP.Shutdown(ctx)
+	if err != nil {
+		return fmt.Errorf("error shutting down http server: %v", err)
+	}
+
+	return nil
+}
